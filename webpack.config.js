d52e73afd962a62420a08180a03f565c808cc844
chakkasailesh <chakkasailesh444@gmail.com>
2023-12-23 09:59:23 +0530

    initial project setup

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  //This property defines where the application starts
+  entry: './src/index.js',
+
+  //This property defines the file path and the file name which will be used for deploying the bundled file
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+  },
+
+  //Setup loaders
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+
+  // Setup plugin to use a HTML file for serving bundled js files
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+}
